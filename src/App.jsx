cce640906c8cde807207955600024bf30cc6d16d
Justamanuelax <justamanuelax@gmail.com>
2025-09-22 10:42:40 +0200

    Revert "Did the restart Timer and the Restart Modal works now"
    This reverts commit 0ca950d2db0e0a8a25af80321d252aa4334cc5ce.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,9 @@ import './App.scss'
 import StartGame, {gameTime} from "./StartGame.jsx";
 import GameTimer from "./GameTimer.jsx";
 import Basket from "./Basket.jsx";
-import RestartGame from './RestartGame.jsx';
-import { useState } from 'react';
-
 function App() {
-    // state for the score
-    const [score, setScore] = useState(0);
-  
   // destructure the time, gameStarted and startTimer from the GameTimer
-    const {time,gameStarted,startTimer,gameOver, resetGame} = GameTimer(gameTime);
+    const {time,gameStarted,startTimer} = GameTimer(gameTime);
     // return the App component
   return (
     // return the App component
@@ -19,8 +13,6 @@ function App() {
         <span className="time">Time: {time}</span>
         {/* display the StartGame component if the game is not started */}
         {!gameStarted && <StartGame startTimer={startTimer} />}
-        {/* display the GameOver component if the game is over */}
-        {gameOver && <RestartGame setScore={setScore} startTimer={startTimer} score={score} resetGame={resetGame}/>}
         {/* display the Basket component */}
         <Basket x={700} width={500} height={100}/>
         {/* Hardcoded the x , width and height for now (proof of concept works) */}
